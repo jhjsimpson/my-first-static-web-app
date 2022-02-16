580dcfa80717c23d325bf1ef2fe293fd3b56f818
Jon <jhjsimpson@gmail.com>
2022-02-16 11:35:59 +0000

    Keith API workaround

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+// import React, { useState, useEffect } from 'react';
+
+// function App() {
+//   const [data, setData] = useState('');
+
+//   useEffect(() => {
+//     fetch("/api/message")
+//     .then(res => res.text())
+//     .then(data => setData(data));      
+//     }, []);
+
+//   return ( <div>{data}</div> );
+// }
+
+// export default App;
+
+// ----- The code (provided in the quickstart) throws an error and does not render text to the page ----- //
+
+
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { responseMessage } = await( await fetch(`/api/message`)).json();
+      setData(responseMessage);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
+
